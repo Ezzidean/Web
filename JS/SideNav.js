1fc32017f42abb9c3a0a6eeced2ccdad4bd935f8
unknown <mrlocal@RLLC-046.ad.mtroyal.ca>
2024-08-07 12:11:11 -0600

    Nav bar does not work

--- JS/SideNav.js
+++ JS/SideNav.js
@@ -1,12 +1,12 @@
-let toggle = button => {
-    let element = document.getElementById("mydiv");
-    let hidden = element.getAttribute("hidden");
-
-    if (hidden) {
-       element.removeAttribute("hidden");
-       button.innerText = "Hide div";
-    } else {
-       element.setAttribute("hidden", "hidden");
-       button.innerText = "Show div";
-    }
-  }
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+   document.getElementById("mySidebar").style.width = "0px";
+   document.getElementById("main").style.marginLeft = "0px";
+ }
+ 
+ /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+ function closeNav() {
+   document.getElementById("mySidebar").style.width = "0";
+   document.getElementById("main").style.marginLeft = "0";
+ }
+ 
